36eff071a63506c15ecf290e7faba939cf546466
PSPDFKit <support@pspdfkit.com>
2024-11-18 17:27:18 +0100

    PSPDFKit for Web Electron Example

---
+++ scripts/copy-pspdfkit-files.js
@@ -0,0 +1,12 @@
+const ncp = require("ncp").ncp;
+
+ncp(
+  "./node_modules/pspdfkit/dist/pspdfkit-lib",
+  "./src/pspdfkit-lib",
+  (err) => {
+    err && console.error(err);
+  }
+);
+ncp("./node_modules/pspdfkit/dist/pspdfkit.js", "./src/pspdfkit.js", (err) => {
+  err && console.error(err);
+});
